d603bb4abab17f9d12f847f4e861afbbc0806217
Jacob Gasper <jacobgasper@gmail.com>
2021-09-12 16:28:01 -0400

    basic graph functionality

--- assets/js/cryptoScript.js
+++ assets/js/cryptoScript.js
@@ -1,31 +1,183 @@
-function searchCrypto() {
+let cryptoSearchText;
 
-    let cryptoSearchText = prompt("Enter cryptocurrency:")
-    let cryptoSearchTime = prompt("Enter timeframe: \n24h, 1w, 1m, 3m, 6m, 1y, all")
+let chartPeriod;
 
-    let requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+let cryptoChart;
 
-    fetch("https://api.coinstats.app/public/v1/charts?period=" + cryptoSearchTime + "&coinId=" + cryptoSearchText, requestOptions)
-        .then(function (response) {
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function (data) {
-                    console.log(data);
-                });
-            } else {
-                console.log('response', response);
-                alert('Error: ' + response.statusText);
-    
-            }
-        })
-        .catch(function (error) {
-            alert('Unable to connect');
-        });
+// let crypto24hPrice = [];
+// let crypto1wPrice = [];
+// let crypto1mPrice = [];
+// let crypto3mPrice = [];
+// let crypto1yPrice = [];
 
+
+let requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+let oneDayEl = document.getElementById("oneDay");
+let oneWeekEl = document.getElementById("oneWeek");
+let oneMonthEl = document.getElementById("oneMonth");
+let threeMonthEl = document.getElementById("threeMonth");
+let oneYearEl = document.getElementById("oneYear");
+
+function searchCrypto(key) {
+
+    if (key.keyCode == 13) {
+
+        cryptoSearchText = document.getElementById("cryptoSearch").value;
+
+        fetch("https://api.coinstats.app/public/v1/coins/" + cryptoSearchText + "?currency=USD", requestOptions)
+            .then(function (response) {
+                if (response.ok) {
+                    console.log(response);
+                    response.json().then(function (data) {
+                        console.log(data);
+
+                        clearChart();
+
+                        let shortenedPrice = data.coin.price.toFixed(2);
+                        document.getElementById("cryptoTitle").textContent = "Rank #" + data.coin.rank + " " + data.coin.name + " " + data.coin.symbol + " $" + shortenedPrice;
+
+                    });
+                } else {
+                    console.log('response', response);
+                    alert('Error: ' + response.statusText);
+
+                }
+            })
+            .catch(function (error) {
+                alert('Unable to connect');
+            });
+    }
 }
 
-searchCrypto();
 
+
+
+function setTimeFrame() {
+    console.log(chartPeriod, cryptoSearchText);
+    fetch("https://api.coinstats.app/public/v1/charts?period="+ chartPeriod + "&coinId=" + cryptoSearchText, requestOptions)
+    .then(function (response) {
+        if (response.ok) {
+            console.log("*********")
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+
+                crypto24hPrice = [];
+                crypto1wPrice = [];
+                crypto1mPrice = [];
+                crypto3mPrice = [];
+                crypto1yPrice = [];
+
+                if (chartPeriod === "24h") {
+                    for (i = 0; i < data.chart.length; i++) {
+                        crypto24hPrice.push(data.chart[i][1]);
+                    }
+                    cryptoTimeFrame = crypto24hPrice;
+                } else if (chartPeriod === "1w") {
+                    for (i = 0; i < data.chart.length; i++) {
+                        crypto1wPrice.push(data.chart[i][1]);
+                    }
+                    cryptoTimeFrame = crypto1wPrice;
+                } else if (chartPeriod === "1m") {
+                    for (i = 0; i < data.chart.length; i++) {
+                        crypto1mPrice.push(data.chart[i][1]);
+                    }
+                    cryptoTimeFrame = crypto1mPrice;
+                } else if (chartPeriod === "3m") {
+                    for (i = 0; i < data.chart.length; i++) {
+                        crypto3mPrice.push(data.chart[i][1]);
+                    }
+                    cryptoTimeFrame = crypto3mPrice;
+                } else if (chartPeriod === "1y") {
+                    for (i = 0; i < data.chart.length; i++) {
+                        crypto1yPrice.push(data.chart[i][1]);
+                    }
+                    cryptoTimeFrame = crypto1yPrice;
+                }
+               
+                generateChart(cryptoTimeFrame)
+
+            });
+        } else {
+            console.log('response', response);
+            alert('Error: ' + response.statusText);
+
+        }
+    })
+    .catch (function(error) {
+        alert('Unable to connect');
+    });
+}
+
+function generateChart(chart){
+
+    clearChart();
+
+    let myChart = document.getElementById('myChart').getContext("2d");
+
+    let cryptoChart = new Chart(myChart, {
+        type: "line", //bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data: {
+            labels: chart,
+            datasets: [{
+                label: "Price",
+                data: chart,
+                backgroundColor: "green",
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderWidth: 1,
+                borderColor: "#777",
+                hoverBorderWidth: 3,
+                hoverBorderColor: "#000"
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+        }
+    });
+}
+
+function clearChart(){
+    let containerEl = document.getElementById("chartContainer");
+    let chartEl = document.getElementById("myChart");
+    let createChart = document.createElement("canvas");
+    chartEl.parentNode.removeChild(chartEl);
+    containerEl.appendChild(createChart);
+    createChart.id = "myChart";
+}
+
+oneDayEl.addEventListener("click", function (event){
+    event.preventDefault()
+    chartPeriod = "24h";
+    setTimeFrame();
+})
+oneWeekEl.addEventListener("click", function (event){
+    event.preventDefault()
+    chartPeriod = "1w";
+    setTimeFrame();
+})
+oneMonthEl.addEventListener("click", function (event){
+    event.preventDefault()
+    chartPeriod = "1m";
+    setTimeFrame();
+})
+threeMonthEl.addEventListener("click", function (event){
+    event.preventDefault()
+    chartPeriod = "3m";
+    setTimeFrame();
+})
+oneYearEl.addEventListener("click", function (event){
+    event.preventDefault()
+    chartPeriod = "1y";
+    setTimeFrame();
+})
--- assets/js/stock.js
+++ assets/js/stock.js
@@ -4,7 +4,17 @@ let stockTitleEl = document.querySelector(".stockTitle");
 let stockInfoEl = document.querySelector(".stockInfoEl");
 let apiKey = "75c3119c360260691b306d8841a20c7d";
 
+let oneDayEl = document.getElementById("oneDay");
+let oneWeekEl = document.getElementById("oneWeek");
+let oneMonthEl = document.getElementById("oneMonth");
+let threeMonthEl = document.getElementById("threeMonth");
+let oneYearEl = document.getElementById("oneYear");
 
+let chartPeriod;
+let stockTicker = "";
+
+let dateFrom;
+let dateTo;
 
 //  75c3119c360260691b306d8841a20c7d  -marketstack api
 
@@ -19,7 +29,7 @@ let apiKey = "75c3119c360260691b306d8841a20c7d";
     function formSubmit(event) {
       event.preventDefault();
       let input = submitElement.value;
-      console.log(input);
+      stockTicker = submitElement.value;
       
 
       fetch('http://api.marketstack.com/v1/tickers?access_key='+apiKey+'&symbols='+input+'', {
@@ -33,7 +43,7 @@ let apiKey = "75c3119c360260691b306d8841a20c7d";
         //console.log(response);
         response.json().then(function (data) {
             console.log(data);
-            console.log(data.data[0].name);
+            
             
             stockTitleEl.textContent = data.data[0].name;
         
@@ -52,7 +62,38 @@ let apiKey = "75c3119c360260691b306d8841a20c7d";
 });
 
     
-  //fetch API again to get stock price.
+  
+
+
+fetch('http://api.marketstack.com/v1/eod?access_key='+apiKey+'&symbols='+input+'', {
+    // The browser fetches the resource from the remote server without first looking in the cache.
+    // The browser will then update the cache with the downloaded resource.
+    cache: 'reload',
+  })
+
+  .then(function (response) {
+    if (response.ok) {
+        
+        response.json().then(function (data) {
+            console.log(data);
+            console.log(data.data[0].adj_close);
+            
+            stockTitleEl.textContent = stockTitleEl.textContent + " $" + data.data[0].adj_close;
+        
+          
+          });
+    
+    
+      } else {
+        
+        alert('Error: ' + response.statusText);
+
+    }
+})
+.catch(function (error) {
+    alert(error + ' Unable to connect');
+});
+
 
 
 
@@ -60,8 +101,230 @@ let apiKey = "75c3119c360260691b306d8841a20c7d";
 }
 
 
+
+
+// send array of price data to generate chart
+function generateChart(chart){
+
+  clearChart();
+
+  let myChart = document.getElementById('myChart').getContext("2d");
+
+ 
+ 
+  let stockChart = new Chart(myChart, {
+      type: "line", //bar, horizontalBar, pie, line, doughnut, radar, polarArea
+      data: {
+          labels: chart,
+          datasets: [{
+              label: "Price",
+              data: chart,
+              backgroundColor: "green",
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderWidth: 1,
+              borderColor: "#777",
+              hoverBorderWidth: 3,
+              hoverBorderColor: "#000"
+          }],
+      },
+      options: {
+          maintainAspectRatio: false,
+      }
+  });
+}
+
+function clearChart(){
+  let containerEl = document.getElementById("chartContainer");
+  let chartEl = document.getElementById("myChart");
+  let createChart = document.createElement("canvas");
+  chartEl.parentNode.removeChild(chartEl);
+  containerEl.appendChild(createChart);
+  createChart.id = "myChart";
+}
+
+function getDateRange(chartPeriod) {
+  let date = new Date();
+
+  let fullYear;
+  let month;
+  let day;
+  
+  if (chartPeriod == "1y") {
+    fullYear = date.getFullYear();
+    month = date.getMonth() + 1;
+    if (month<10) {
+      month = "0"+month;
+    }
+    day = date.getDate();
+
+    if (day<10) {
+      day = "0"+day;
+    }
+
+    dateFrom = fullYear-1 + "-" + month + "-" + day;
+    dateTo = fullYear + "-" + month + "-" + day;
+
+  }
+
+    //may cause error if 3months ago is a shorter month ex. 30th of one month, but previous month only has 28 days
+  else if (chartPeriod == "3m") {
+    fullYear = date.getFullYear();
+    month = date.getMonth() + 1;
+    let preMonth = date.getMonth() + 1 -3;
+    if (month<10) {
+      month = "0"+month;
+    }
+
+    if (preMonth<10) {
+      preMonth = "0"+preMonth;
+    }
+    day = date.getDate();
+
+    if (day<10) {
+      day = "0"+day;
+    }
+
+    dateFrom = fullYear + "-" + preMonth + "-" + day;
+    dateTo = fullYear + "-" + month + "-" + day;
+
+  }
+
+  //can have same error as 3m where a longer month won't have the same day as a shorter month
+  else if (chartPeriod == "1m") {
+    fullYear = date.getFullYear();
+    month = date.getMonth() + 1;
+    let preMonth = date.getMonth() + 1 -1;
+    if (month<10) {
+      month = "0"+month;
+    }
+
+    if (preMonth<10) {
+      preMonth = "0"+preMonth;
+    }
+    day = date.getDate();
+
+    if (day<10) {
+      day = "0"+day;
+    }
+
+    dateFrom = fullYear + "-" + preMonth + "-" + day;
+    dateTo = fullYear + "-" + month + "-" + day;
+
+  }
+
+  else if (chartPeriod == "1w") {
+    fullYear = date.getFullYear();
+    month = date.getMonth() + 1;
+    let preDay;
+    if (month<10) {
+      month = "0"+month;
+    }
+    day = date.getDate();
+    preDay = day-7;
+
+    if (day<10) {
+      day = "0"+day;
+    }
+
+    if (preDay<10) {
+      preDay = "0"+preDay;
+    }
+
+    dateFrom = fullYear + "-" + month + "-" + preDay;
+    dateTo = fullYear + "-" + month + "-" + day;
+
+  }
+
+  else if (chartPeriod == "24h") {
+    fullYear = date.getFullYear();
+    month = date.getMonth() + 1;
+    let weekDay = date.getDay();
+    console.log("day of week " + weekDay);
+    let preDay;
+    if (month<10) {
+      month = "0"+month;
+    }
+    day = date.getDate()-1;
+    preDay = day-2;
+
+    if (day<10) {
+      day = "0"+day;
+    }
+
+    if (preDay<10) {
+      preDay = "0"+preDay;
+    }
+
+    dateFrom = fullYear + "-" + month + "-" + preDay;
+    dateTo = fullYear + "-" + month + "-" + day;
+
+  }
+
+
+
+
+}
     
     
+ //adjust to works with stock API
+function setTimeFrame() {
+  
+  
+  getDateRange(chartPeriod);
+
+  console.log("date check " + dateFrom + "/" + dateTo);
+
+  //get proper dates for time period
+  
+  
+  
+  fetch('http://api.marketstack.com/v1/eod?access_key='+apiKey+'&symbols='+stockTicker+'&date_from='+dateFrom+'&date_to='+dateTo+'&limit=100',)
+  .then(function (response) {
+      if (response.ok) {
+          console.log("*********")
+          console.log(response);
+          response.json().then(function (data) {
+              console.log(data);
+
+              stockPricesArray = [];
+
+              console.log(data.data[0].adj_close)
+              console.log(data.data.length);
+
+              for (let i = 0; i <data.data.length; i++) {
+
+                stockPricesArray.push(data.data[i].adj_close);
+
+              }
+             
+             
+              generateChart(stockPricesArray)
+
+          });
+      } else {
+          console.log('response', response);
+          alert('Error: ' + response.statusText);
+
+      }
+  })
+  .catch (function(error) {
+      alert('Unable to connect');
+  });
+}
+
+
+
+
+
+
+
     
     //event listener to get stock ticker symbol from page, then fetch API
 
@@ -70,3 +333,28 @@ let apiKey = "75c3119c360260691b306d8841a20c7d";
     //add event listeners for one day/week/month/year buttons
 
 
+  oneDayEl.addEventListener("click", function (event){
+      event.preventDefault()
+      chartPeriod = "24h";
+      setTimeFrame();
+  })
+  oneWeekEl.addEventListener("click", function (event){
+      event.preventDefault()
+      chartPeriod = "1w";
+      setTimeFrame();
+  })
+  oneMonthEl.addEventListener("click", function (event){
+      event.preventDefault()
+      chartPeriod = "1m";
+      setTimeFrame();
+  })
+  threeMonthEl.addEventListener("click", function (event){
+      event.preventDefault()
+      chartPeriod = "3m";
+      setTimeFrame();
+  })
+  oneYearEl.addEventListener("click", function (event){
+      event.preventDefault()
+      chartPeriod = "1y";
+      setTimeFrame();
+})
